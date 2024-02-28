4ad88250f5ec7733cfa8f149ef2d545a88499e25
Hitesh-Yadav2616 <yhitesh230@gmail.com>
2024-02-28 09:39:24 -0800

    modified with classes

--- Assignment1c++.cpp
+++ Assignment1c++.cpp
@@ -4,125 +4,139 @@
 
 using namespace std;
 
-// Define a structure for bank account
-struct BankAccount {
+class BankAccount {
+private:
     string password;
     double balance;
+
+public:
+    BankAccount() : password(""), balance(0.0) {}
+
+    BankAccount(const string& pass, double bal) : password(pass), balance(bal) {}
+
+    string getPassword() const {
+        return password;
+    }
+
+    double getBalance() const {
+        return balance;
+    }
+
+    void setPassword(const string& pass) {
+        password = pass;
+    }
+
+    void deposit(double amount) {
+        balance += amount;
+    }
+
+    void withdraw(double amount) {
+        balance -= amount;
+    }
 };
 
-// Global map to store user IDs and corresponding bank accounts
-map<string, BankAccount> accounts;
+class ATM {
+private:
+    map<string, BankAccount> accounts;
 
-// Function prototypes
-void printIntroMenu();
-void printMainMenu();
-void start();
-void login();
-void createAccount();
+public:
+    void printIntroMenu() {
+        cout << "Please select an option from the menu below:\n"
+             << "l -> Login\n"
+             << "c -> Create New Account\n"
+             << "q -> Quit\n"
+             << "> ";
+    }
+
+    void printMainMenu() {
+        cout << "d -> Deposit Money\n"
+             << "w -> Withdraw Money\n"
+             << "r -> Request Balance\n"
+             << "q -> Quit\n"
+             << "> ";
+    }
+
+    void start() {
+        char choice;
+        do {
+            cout << "Hi! Welcome to Mr. Zamar’s ATM Machine!\n";
+            printIntroMenu();
+            cin >> choice;
+            switch(choice) {
+                case 'l':
+                    login();
+                    break;
+                case 'c':
+                    createAccount();
+                    break;
+                case 'q':
+                    cout << "Thanks for stopping by!\n";
+                    exit(0);
+                default:
+                    cout << "Invalid option. Please try again.\n";
+            }
+        } while(choice != 'q');
+    }
+
+    void login() {
+        char choice;
+        double money;
+        string userId, password;
+        cout << "Please enter your user id: ";
+        cin >> userId;
+        cout << "Please enter your password: ";
+        cin >> password;
+
+        if(accounts.find(userId) != accounts.end() && accounts[userId].getPassword() == password) {
+            cout << "Access Granted!\n";
+            BankAccount& acc = accounts[userId];
+            do {
+                printMainMenu();
+                cin >> choice;
+                switch(choice) {
+                    case 'd':
+                        cout << "Amount of deposit: $";
+                        cin >> money;
+                        acc.deposit(money);
+                        cout << "Amount deposited!\n";
+                        break;
+                    case 'w':
+                        cout << "Amount of withdrawal: $";
+                        cin >> money;
+                        acc.withdraw(money);
+                        break;
+                    case 'r':
+                        cout << "\nYour balance: $" << acc.getBalance() << endl;
+                        break;
+                    default:
+                        cout << "Enter valid option!\n";
+                }
+            } while(choice != 'q');
+        } else {
+            cout << "*** LOGIN FAILED! ***\n";
+        }
+    }
+
+    void createAccount() {
+        string userId, password;
+        cout << "Please enter your user id: ";
+        cin >> userId;
+        cout << "Please enter your password: ";
+        cin >> password;
+
+        // Check if the user id already exists
+        if(accounts.find(userId) == accounts.end()) {
+            BankAccount newAccount(password, 0.0);
+            accounts[userId] = newAccount;
+            cout << "Thank You! Your account has been created!\n";
+        } else {
+            cout << "User ID already exists. Please try again with a different ID.\n";
+        }
+    }
+};
 
 int main() {
-    start();
+    ATM atm;
+    atm.start();
     return 0;
-}
-
-void printIntroMenu() {
-    cout << "Please select an option from the menu below:\n"
-         << "l -> Login\n"
-         << "c -> Create New Account\n"
-         << "q -> Quit\n"
-         << "> ";
-}
-
-void printMainMenu() {
-    cout << "d -> Deposit Money\n"
-         << "w -> Withdraw Money\n"
-         << "r -> Request Balance\n"
-         << "q -> Quit\n"
-         << "> ";
-}
-
-void start() {
-    char choice;
-    do {
-        cout << "Hi! Welcome to Mr. Zamar’s ATM Machine!\n";
-        printIntroMenu();
-        cin >> choice;
-        switch(choice) {
-            case 'l':
-                login();
-                break;
-            case 'c':
-                createAccount();
-                break;
-            case 'q':
-                cout << "Thanks for stopping by!\n";
-                exit(0);
-            default:
-                cout << "Invalid option. Please try again.\n";
-        }
-    } while(choice != 'q');
-}
-
-void login() {
-    char choice;
-    double money;
-    string userId, password;
-    cout << "Please enter your user id: ";
-    cin >> userId;
-    cout << "Please enter your password: ";
-    cin >> password;
-
-    if(accounts.find(userId) != accounts.end() && accounts[userId].password == password) {
-        cout << "Access Granted!\n";
-        BankAccount dep;
-        do {
-            printMainMenu();
-        cin >> choice;
-        switch(choice) {
-            case 'd':
-            cout << "Amount of deposit: $";
-            cin >> money;
-            dep.balance += money;
-            accounts[userId] = dep;
-            cout << "Amount deposited!\n";
-            break;
-
-            case 'w':
-            cout << "Amount of withdrawl: $";
-            cin >> money;
-            dep.balance -= money;
-            accounts[userId] = dep;
-            break;
-
-            case 'r':
-            cout << "\nYour balance: $" << accounts[userId].balance << endl;
-            break;
-
-            default : cout << "Enter valid option!";
-
-        }
-        } while(choice != 'q');
-        
-    } else {
-        cout << "*** LOGIN FAILED! ***\n";
-    }
-}
-
-void createAccount() {
-    string userId, password;
-    cout << "Please enter your user id: ";
-    cin >> userId;
-    cout << "Please enter your password: ";
-    cin >> password;
-
-    // Check if the user id already exists
-    if(accounts.find(userId) == accounts.end()) {
-        BankAccount newAccount;
-        newAccount.password = password;
-        newAccount.balance = 0.0;
-        accounts[userId] = newAccount;
-        cout << "Thank You! Your account has been created!\n";
-    } else {
-        cout << "User ID already exists. Please try again with a different ID.\n";
-    }
 }
